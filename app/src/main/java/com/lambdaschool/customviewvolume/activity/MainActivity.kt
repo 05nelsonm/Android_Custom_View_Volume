95ee525538cf96bfccbf0904f7294af80b8402e8
Matthew Nelson <05nelsonm@protonmail.com>
2019-08-22 23:13:35 -0400

    added rotation upon user interaction and toasting of the volume percentage

--- app/src/main/java/com/lambdaschool/customviewvolume/activity/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/customviewvolume/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lambdaschool.customviewvolume
+package com.lambdaschool.customviewvolume.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lambdaschool.customviewvolume.R
 
 class MainActivity : AppCompatActivity() {
 
